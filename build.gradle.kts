5428365ddabf906569b1942c9646f57648af8453
Duong Hong An <themasterclaus@gmail.com>
2022-06-03 16:29:42 +0700

    tab

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,51 +2,51 @@ group = "net.minevn"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    id("java")
+	id("java")
 }
 
 repositories {
-    mavenCentral()
-    mavenLocal()
-    maven {
-        setUrl("http://pack.minevn.net/repo/")
-        isAllowInsecureProtocol = true
-    }
+	mavenCentral()
+	mavenLocal()
+	maven {
+		setUrl("http://pack.minevn.net/repo/")
+		isAllowInsecureProtocol = true
+	}
 
 }
 
 dependencies {
-    compileOnly("minevn.depend:paper:1.12.2-b1619")
-    implementation("org.jetbrains:annotations:23.0.0")
+	compileOnly("minevn.depend:paper:1.12.2-b1619")
+	implementation("org.jetbrains:annotations:23.0.0")
 }
 
 tasks {
-    val jarName = "GuiAPI"
-    var originalName = ""
-    val path = project.properties["shadowPath"]
+	val jarName = "GuiAPI"
+	var originalName = ""
+	val path = project.properties["shadowPath"]
 
-    jar {
-        originalName = archiveFileName.get()
-    }
+	jar {
+		originalName = archiveFileName.get()
+	}
 
-    register("customCopy") {
-        dependsOn(jar)
+	register("customCopy") {
+		dependsOn(jar)
 
-        doLast {
-            if (path != null) {
-                println("Copying $originalName to $path")
-                val to = File("$path/$originalName")
-                val rename = File("$path/$jarName.jar")
-                File(project.projectDir, "build/libs/$originalName").copyTo(to, true)
-                if (rename.exists()) rename.delete()
-                to.renameTo(rename)
-                println("Copied")
-            }
-        }
-    }
+		doLast {
+			if (path != null) {
+				println("Copying $originalName to $path")
+				val to = File("$path/$originalName")
+				val rename = File("$path/$jarName.jar")
+				File(project.projectDir, "build/libs/$originalName").copyTo(to, true)
+				if (rename.exists()) rename.delete()
+				to.renameTo(rename)
+				println("Copied")
+			}
+		}
+	}
 
 
-    assemble {
-        dependsOn(get("customCopy"))
-    }
+	assemble {
+		dependsOn(get("customCopy"))
+	}
 }
